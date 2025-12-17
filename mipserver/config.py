68cd8976f0e17a457b0a5670d474ae858e1e17e0
Henning Thieß <ht@xomox.cc>
2025-12-17 15:31:06 +0100

    Refactor logging configuration in config.py: improve log format, add skiplog filter, and enhance logger setup

--- mipserver/config.py
+++ mipserver/config.py
@@ -17,12 +17,16 @@ from pydantic_core.core_schema import ValidationInfo
 
 os.environ["LOGURU_LEVEL"] = os.getenv("LOGURU_LEVEL", "DEBUG")  # standard is DEBUG
 logger.remove()  # remove default-handler
-logger_fmt: str = "<g>{time}</> | <lvl>{level}</> | <c>{extra[classname]}:{function}:{line}</> - {message}"
-# # https://buildmedia.readthedocs.org/media/pdf/loguru/latest/loguru.pdf
-logger.add(
-    sys.stderr, level=os.getenv("LOGURU_LEVEL", "DEBUG"), format=logger_fmt
-)  # TRACE | DEBUG | INFO | WARN | ERROR |  FATAL
-logger.configure(extra={"classname": "None"})
+
+logger_fmt: str = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{module}</cyan>::<cyan>{extra[classname]}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+# logger_fmt: str = "<g>{time:HH:mm:ssZZ}</> | <lvl>{level}</> | <c>{module}::{extra[classname]}:{function}:{line}</> - {message}"
+
+def _loguru_skiplog_filter(record: dict) -> bool:
+    return not record.get("extra", {}).get("skiplog", False)
+
+logger.add(sys.stderr, level=os.getenv("LOGURU_LEVEL"), format=logger_fmt, filter=_loguru_skiplog_filter)  # type: ignore # TRACE | DEBUG | INFO | WARN | ERROR |  FATAL
+logger.configure(extra={"classname": "None", "skiplog": False})
+
 
 
 _CONFIGDIRPATH: Path = Path(__file__).parent.resolve()
